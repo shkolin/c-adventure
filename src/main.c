64693b06da635a6796747975da68bda8bac33b94
Vitalii Shkolin <vitalii@shkolin.net.ua>
2026-01-30 22:03:22 +0200

    Refactor main for new Player struct and game loop

--- src/main.c
+++ src/main.c
@@ -1,99 +1,78 @@
-#include "raylib.h"
+#include "main.h"
+#include "player.h"
+#include <raylib.h>
 #include <stdio.h>
-
-typedef struct Player {
-  Vector2 position;
-  float speed;
-} Player;
-
-// Module functions declaration
-static void init_game(void);
-static void update_game(void);
-static void draw_game(void);
 
 // Module variables declaration (local)
 static Player player = {0};
 static Texture2D player_texture = {0};
-static RenderTexture2D render_target = {0};
+static Texture2D player_texture_idle = {0};
+static Camera2D camera = {0};
 
-static const int SCREEN_WIDTH = 432;
-static const int SCREEN_HEIGHT = 270;
-static const int SCALE = 3;
+static const int SCREEN_WIDTH = 800;
+static const int SCREEN_HEIGHT = 600;
 static const int FPS = 60;
 static const int FONT_SIZE = 14;
 
 // Main entry point
 int main(void) {
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-  InitWindow(SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE, "My Game");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "C Adventure");
   SetTargetFPS(FPS);
 
-  init_game();
+  InitGame();
 
   while (!WindowShouldClose()) {
-    update_game();
-    draw_game();
+    UpdateGame();
+    DrawGame();
   }
 
-  UnloadRenderTexture(render_target);
   UnloadTexture(player_texture);
+  UnloadTexture(player_texture_idle);
 
   CloseWindow();
   return 0;
 }
 
 // Initialize game variables
-static void init_game(void) {
-  render_target = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
-  SetTextureFilter(render_target.texture, TEXTURE_FILTER_POINT);
+static void InitGame(void) {
   // Initialize player variables
   player.position.x = 0;
   player.position.y = 0;
-  player.speed = 1.0f;
+  player.size.width = 16;
+  player.size.height = 16;
+  player.speed = DEFAULT_PLAYER_SPEED;
+  player.state = Idle;
 
-  Image player_image = LoadImage("../assets/character.png");
-  player_texture = LoadTextureFromImage(player_image);
-
-  UnloadImage(player_image);
+  player_texture = LoadTexture("../assets/Char_003.png");
+  player_texture_idle = LoadTexture("../assets/Char_003_Idle.png");
 }
 
-static void update_game(void) {
+static void UpdateGame(void) {
   // Player movement
-  if (IsKeyDown(KEY_W)) {
+  if (IsKeyDown(KEY_W) && player.position.y > 0) {
+    player.direction = UP;
     player.position.y -= player.speed;
   }
-  if (IsKeyDown(KEY_S)) {
+  if (IsKeyDown(KEY_S) &&
+      player.position.y + player.size.height < SCREEN_HEIGHT) {
+    player.direction = DOWN;
     player.position.y += player.speed;
   }
-  if (IsKeyDown(KEY_A)) {
+  if (IsKeyDown(KEY_A) && player.position.x > 0) {
+    player.direction = LEFT;
     player.position.x -= player.speed;
   }
-  if (IsKeyDown(KEY_D)) {
+  if (IsKeyDown(KEY_D) &&
+      player.position.x + player.size.width < SCREEN_WIDTH) {
+    player.direction = RIGHT;
     player.position.x += player.speed;
-  }
-  // Movement bounds
-  if (player.position.x < -2) {
-    player.position.x = -2;
-  }
-
-  if (player.position.y < 0) {
-    player.position.y = 0;
   }
 }
 
 // Draw game elements
-static void draw_game(void) {
-  BeginTextureMode(render_target);
-  ClearBackground(DARKGRAY);
-  DrawTexture(player_texture, player.position.x, player.position.y, WHITE);
-  DrawFPS(10, 10);
-  EndTextureMode();
-
+static void DrawGame(void) {
   BeginDrawing();
-  ClearBackground(BLACK);
-  DrawTexturePro(render_target.texture,
-                 (Rectangle){0, 0, SCREEN_WIDTH, -SCREEN_HEIGHT},
-                 (Rectangle){0, 0, SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE},
-                 (Vector2){0, 0}, 0.0f, WHITE);
+  ClearBackground(DARKGRAY);
+  DrawFPS(10, 10);
   EndDrawing();
 }
